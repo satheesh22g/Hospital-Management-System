9b1c1dae0aab5841251a25b8c589a1c7f3f9130f
kaushal <kaushalbrahmbhatt38@gmail.com>
2020-06-27 14:13:36 +0530

    flask cli command created

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 import os
-import io
-import sys
-from flask import send_file
 from flask import Flask, session, render_template, request, redirect, url_for, flash, jsonify, Response
 from flask_bcrypt import Bcrypt
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from database import Base,Users,Patients
 from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import scoped_session, sessionmaker
-import datetime
-import xlwt
-from fpdf import FPDF
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
@@ -20,8 +15,54 @@ app.secret_key = os.urandom(24)
 engine = create_engine('sqlite:///database.db',connect_args={'check_same_thread': False},echo=True)
 Base.metadata.bind = engine
 db = scoped_session(sessionmaker(bind=engine))
+
+# Flask CLI Commands for database initialize, load and drop
+
+# run flask db_create command into terminal for create .db file
+@app.cli.command('db_create')
+def db_create():
+    SQLAlchemy(app).create_all()
+    print('Database Created.')
+
+# run flask db_drop command into terminal for drop all table from .db file
+@app.cli.command('db_drop')
+def db_drop():
+    SQLAlchemy(app).drop_all()
+    print('Database droped.')
+
+# run flask db_seedData command into terminal for feed the initial user table data
+# which make you able to login 
+@app.cli.command('db_seedData')
+def db_seedData():
+    desk_executive = Users(
+        id = 'C00000001',
+        name = 'ramesh',
+        user_type = 'RDE',
+        password = bcrypt.generate_password_hash('Ramesh@001').decode('utf-8')
+    )
+
+    pharmacist = Users(
+        id = 'C00000002',
+        name = 'suresh',
+        user_type = 'pharmacist',
+        password = bcrypt.generate_password_hash('Suresh@002').decode('utf-8')
+    )
+
+    diagnostics_executive = Users(
+        id = 'C00000003',
+        name = 'mahesh',
+        user_type = 'DSE',
+        password = bcrypt.generate_password_hash('Mahesh@003').decode('utf-8')
+    )
+
+    db.add(desk_executive)
+    db.add(pharmacist)
+    db.add(diagnostics_executive)
+
+    db.commit()
+    print('Inital data added.')
     
-# MAIN
+# all route starts from here
 @app.route('/')
 @app.route("/dashboard")
 def dashboard():
